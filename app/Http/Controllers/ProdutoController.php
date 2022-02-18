1b6f09e391d0499d35116a8ef1e70570003404b7
lazarodaniel83 <ldss158@gmail.com>
2022-02-17 23:34:23 -0300

    Finalizando  o cap 11º do Livro de laravel | validando os dado de entrada

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,13 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use e\Http\Request\ProdutoRequest;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\ProdutosRequest;
 use Illuminate\Support\Facades\DB ;
 use App\Produto;
-use Request;
+//use Request;
 
 class ProdutoController extends Controller
 {
+    public function novo(){
+        return view('produto.formulario');
+    }
+
+    public function adiciona(ProdutosRequest $request){
+        //    $nome = Request::input('nome');
+        //    $valor = Request::input('valor');
+        //    $descricao = Request::input('descricao');
+        //    $quantidade = Request::input('quantidade');        
+
+        //    DB::insert('insert into produtos
+        //     (nome, valor, descricao, quantidade) values (?,?,?,?)',
+        //      array($nome,$valor,$descricao,$quantidade));
+
+        //     DB::table('produtos')->insert(['nome'=>$nome,
+        //                 'valor' => $valor,'descricao'=> $descricao,
+        //                 'quantidade' => $quantidade]);
+
+        // $produto = new Produto();
+        // $produto->nome = Request::input('nome');
+        // $produto->valor = Request::input('valor');
+        // $produto->descricao = Request::input('descricao');
+        // $produto->quantidade = Request::input('quantidade');        
+
+        // $params = Request::all();
+        // $produto = new Produto($params);
+        // $produto->save();
+
+        // $validator = Validator::make(
+        //     ['nome' => Request::input('nome')],
+        //     ['nome' => 'required|min:5']
+        // );
+
+        // if($validator = fails()){
+        //     return redirect()
+        //     ->action('ProdutoController@novo');
+        // }
+
+        Produto::create(Request::all);
+
+        return redirect()
+        ->action('ProdutoController@lista')
+        ->withInput(Request::only('nome'));
+   }
+
     public function lista(){
        
         $produtos = Produto::all();
@@ -33,40 +80,7 @@ class ProdutoController extends Controller
         return view('produto.detalhes')->with('p',$produto);
     }
 
-   public function novo(){
-       return view('produto.formulario');
-   }
-
-   public function adiciona(){
-        //    $nome = Request::input('nome');
-        //    $valor = Request::input('valor');
-        //    $descricao = Request::input('descricao');
-        //    $quantidade = Request::input('quantidade');        
-
-        //    DB::insert('insert into produtos
-        //     (nome, valor, descricao, quantidade) values (?,?,?,?)',
-        //      array($nome,$valor,$descricao,$quantidade));
-
-        //     DB::table('produtos')->insert(['nome'=>$nome,
-        //                 'valor' => $valor,'descricao'=> $descricao,
-        //                 'quantidade' => $quantidade]);
-
-        // $produto = new Produto();
-        // $produto->nome = Request::input('nome');
-        // $produto->valor = Request::input('valor');
-        // $produto->descricao = Request::input('descricao');
-        // $produto->quantidade = Request::input('quantidade');        
-
-        // $params = Request::all();
-        // $produto = new Produto($params);
-        // $produto->save();
-
-        Produto::create(Request::all);
-
-        return redirect()
-        ->action('ProdutoController@lista')
-        ->withInput(Request::only('nome'));
-   }
+   
 
    public function listaJson(){
        $produtos = Produto::all();
---
+++ app/Http/Requests/ProdutosRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProdutosRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nome' => 'required|ma:100',
+            'descricao' => 'required|max:255',
+            'valor' => 'required|numeric',
+            'quantidade' => 'required|numeric'
+        ];
+    }
+
+    public function messages(){
+        return [
+            'required' => 'The :attribute field can not be empty',
+        ];
+    }
+}
